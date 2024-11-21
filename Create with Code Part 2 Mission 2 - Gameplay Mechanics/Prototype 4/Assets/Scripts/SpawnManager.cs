d060dfde37dfe1bd57ea78058782e8b3c9b580f0
hehedgehog1 <154544566+hehedgehog1@users.noreply.github.com>
2024-11-21 21:37:58 +0000

    finished unit 4.4

--- Create with Code Part 2 Mission 2 - Gameplay Mechanics/Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Create with Code Part 2 Mission 2 - Gameplay Mechanics/Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public int enemyCount;
+    public int waveNumber;
     public GameObject enemyPrefab;
+    public GameObject powerUpPrefab;
     private float spawnRange = 9;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0 ) 
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber); 
+            Instantiate(powerUpPrefab, GenerateSpawnPosition(), powerUpPrefab.transform.rotation);
+        }
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
 
     private Vector3 GenerateSpawnPosition()
